031a5f73bbb365766e0e1cb23dc6ab54712bbc54
KingKrab23 <47616086+KingKrab23@users.noreply.github.com>
2019-02-17 20:18:05 -0600

    Increment version number

--- GGn Quick Crafting.user.js
+++ GGn Quick Crafting.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GGn Quick Crafter
 // @namespace    http://tampermonkey.net/
-// @version      1.5.0b
+// @version      1.6.0b
 // @description  Craft multiple items easier
 // @author       KingKrab23
 // @match        https://gazellegames.net/user.php?action=crafting
@@ -10,7 +10,7 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js
 // ==/UserScript==
 
-const VERSION = '1.5.0b';
+const VERSION = '1.6.0b';
 
 /* >>>BEGIN<<< User adjustable variables
  * ONLY ADJUST THESE IF YOU KNOW WHAT YOU'RE DOING
