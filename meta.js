4bc58528ff5230a7e80d4f8fa8fb9d84f531db81
FinalDoom <7464170-FinalDoom@users.noreply.gitlab.com>
2022-07-04 03:27:45 -0600

    ➖ remove jQuery

--- meta.js
+++ meta.js
@@ -1,8 +1,5 @@
+exports.name = 'GGn Quick Crafter';
 exports.namespace = 'https://gazellegames.net/';
-exports.require = [
-  'https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js',
-  'https://code.jquery.com/jquery-3.6.0.js',
-  'https://raw.githubusercontent.com/FinalDoom/Quick_Craft/repair-equipped/recipe_info.js',
-];
+exports.require = ['https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js'];
 exports.match = ['https://gazellegames.net/user.php?action=crafting'];
 exports.grant = ['GM.getValue', 'GM.setValue', 'GM.deleteValue'];
--- src/helpers/crafter.ts
+++ src/helpers/crafter.ts
@@ -1,20 +1,24 @@
 import {GeneratedRecipe, ingredients} from '../generated_data/recipe_info';
 
-const authKey = new URLSearchParams($('link[rel="alternate"]').attr('href')).get('authkey');
+const authKey = new URLSearchParams(document.querySelector<HTMLLinkElement>('link[rel="alternate"]').href).get(
+  'authkey',
+);
 const urlBase = (customRecipe) =>
   `https://gazellegames.net/user.php?action=ajaxtakecraftingresult&recipe=${customRecipe}&auth=${authKey}`;
 
 export function take_craft(recipe: GeneratedRecipe) {
   const craftName = recipe.name || ingredients[recipe.itemId].name;
-  $.get(urlBase(recipe.recipe), function (data) {
-    console.log(data);
-    console.log(data.EquipID);
+  fetch(urlBase(recipe.recipe))
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.EquipID);
 
-    if (data === '{}' || data.EquipId !== '') {
-      unsafeWindow.noty({type: 'success', text: `${craftName} was crafted successfully.`});
-    } else {
-      unsafeWindow.noty({type: 'error', text: `${craftName} failed.`});
-      alert(`Crafting failed. Response from server: ${data}`);
-    }
-  });
+      if (data === '{}' || data.EquipId !== '') {
+        unsafeWindow.noty({type: 'success', text: `${craftName} was crafted successfully.`});
+      } else {
+        unsafeWindow.noty({type: 'error', text: `${craftName} failed.`});
+        alert(`Crafting failed. Response from server: ${data}`);
+      }
+    });
 }
