6abcb13a70c9a857c78d6747fd8fa095b3936e14
FinalDoom <7464170-FinalDoom@users.noreply.gitlab.com>
2022-07-17 22:01:55 -0600

    💄 Made shop links reflect availability
    Info icon for ingredients that can't be purchased,
    but can still be used to get info on the item.
    Dollar icon rather than text for purchasable.
    Changed colors to gold/blue

--- src/shop-link/shop-link.tsx
+++ src/shop-link/shop-link.tsx
@@ -1,11 +1,16 @@
+import './shop-link.scss';
 import React from 'react';
+import {HiCurrencyDollar, HiInformationCircle} from 'react-icons/hi';
 
 interface Props {
   ingredientId: number;
+  availableInStore: boolean;
 }
 interface State {}
 
 export default class ShopLink extends React.Component<Props, State> {
+  base = 'crafting-panel-info__ingredient-shop-link';
+
   constructor(props: Props) {
     super(props);
   }
@@ -13,11 +18,11 @@ export default class ShopLink extends React.Component<Props, State> {
   render() {
     return (
       <a
-        className="crafting-panel-info__ingredient-shop-link"
+        className={this.base + (this.props.availableInStore ? ' ' + this.base + '--purchasable' : '')}
         target="_blank"
         href={`https://gazellegames.net/shop.php?ItemID=${this.props.ingredientId}`}
       >
-        $
+        {this.props.availableInStore ? <HiCurrencyDollar size="1rem" /> : <HiInformationCircle size="1rem" />}
       </a>
     );
   }
