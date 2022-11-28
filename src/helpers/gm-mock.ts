736920933f976b2b4dcd144da9e52b87bc647c58
FinalDoom <677609+FinalDoom@users.noreply.github.com>
2022-11-28 11:33:03 -0700

    🔊 Make GM Mock use logger helper

--- src/helpers/gm-mock.ts
+++ src/helpers/gm-mock.ts
@@ -1,16 +1,17 @@
-const background = 'color:black;background-color:chocolate;border-radius:2px;padding:1px;';
-const clear = '';
+import Logger from '../log/log';
+
+const log = Logger.getLogger('GM Mock');
 
 export namespace GMMock {
   export type Value = string | boolean | number;
   export async function getValue<TValue = GM.Value>(name: string, defaultValue?: TValue): Promise<TValue | undefined> {
-    console.log('%cGM Mock:%c getValue(', background, clear, name, defaultValue, ')');
+    log.debug('getValue(', name, defaultValue, ')');
     return defaultValue;
   }
   export async function deleteValue(name: string): Promise<void> {
-    console.log('%cGM Mock:%c deleteValue(', background, clear, name, ')');
+    log.debug('deleteValue(', name, ')');
   }
   export async function setValue(name: string, value: GM.Value): Promise<void> {
-    console.log('%cGM Mock:%c setValue(', background, clear, name, value, ')');
+    log.debug('setValue(', name, value, ')');
   }
 }
